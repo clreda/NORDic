504ef6592012ff4a02996ac78c58889cbf785877
REDA Clemence <clemence.reda@inria.fr>
2023-02-28 21:20:09 +0100

    Fixed test files, one for each part

---
+++ tests/tests_DR.py
@@ -0,0 +1,54 @@
+#coding: utf-8
+
+import NORDic
+import pandas as pd
+from multiprocessing import cpu_count
+import os
+import numpy as np
+
+## IV. Drug Repurposing (**NORDic DR**)
+
+file_folder="ToyOndine/"
+seed_number=12345
+
+np.random.seed(seed_number)
+with open(file_folder+"solution.bnet", "r") as f:
+    genes = [line.split(", ")[0] for line in f.read().split("\n") if (len(line)>0)]
+
+## Create a set of random "patient" and "control" states (see tests_PMR.py)
+states = pd.read_csv(file_folder+"phenotypes.csv", index_col=0)
+### Patient profiles
+patients = states[[c for c in states.columns if ("Case" in c)]].loc[[i for i in states.index if (i != "annotation")]]
+
+## Drug targets
+targets = pd.read_csv(file_folder+"targets.csv", index_col=0)
+
+## Define the function which determines whether a state is closer to controls/patients
+dfdata = states.loc[[i for i in states.index if (i != "annotation")]]
+samples = states.loc["annotation"]
+from NORDic.NORDic_DS.functions import compute_frontier
+## compute_frontier fits a SVM model to the data
+frontier = compute_frontier(dfdata, samples)
+score = lambda attrs : (frontier.predict(attrs.values.T)==1).astype(int)
+
+## Drug Repurposing through adaptive testing
+from NORDic.NORDic_DR.functions import adaptive_testing
+
+BANDIT_args = {
+    'bandit': 'LinGapE', 
+    'seed': seed_number,
+    'delta': 0.1, 
+    'nsimu': 1, 
+    'm': 4, 
+    'c': 0, 
+    'sigma': 1,
+    'beta': "heuristic",
+    'epsilon': 0.1,
+    'tracking_type': "D",
+    'gain_type': "empirical",
+    'learner': "AdaHedge"
+}
+recommendation = adaptive_testing(file_folder+"solution.bnet", targets, targets, score, patients, 
+        SIMU_params, BANDIT_args, reward_fname=None, quiet=False).T
+assert recommendation.shape[0]==targets.shape[1]
+assert np.isclose(recommendation.sum().sum(), BANDIT_args["m"])
--- tests/tests_NI.py
+++ tests/tests_NI.py
@@ -1,12 +1,11 @@
 #coding: utf-8
 
-## Run all the code in NORDic/notebooks/NORDic CoLoMoTo.ipynb
-
 import NORDic
 import pandas as pd
 from multiprocessing import cpu_count
 import os
 import numpy as np
+from subprocess import call
 
 ## I. Building a small regulatory model of CCHS (**NORDic NI**)
 
@@ -38,6 +37,8 @@ BONESIS_args = {"limit": 1, "exact": True, "max_maxclause": 3}
 ## Advanced
 DESIRABILITY = {"DS": 3, "CL": 3, "Centr": 3, "GT": 1}
 
+call("mkdir -p "+file_folder, shell=True)
+
 ## The undirected, unsigned network from STRING
 network_content = pd.DataFrame([], index=["preferredName_A", "preferredName_B", "sign", "directed", "score"])
 network_content[0] = ["PHOX2B", "BDNF", 2, 0, 0.342]
@@ -55,18 +56,19 @@ network_content[11] = ["EDN3", "GDNF", 2, 0, 0.634]
 network_content[12] = ["RET", "BDNF", 2, 0, 0.438]
 network_content[12] = ["RET", "GDNF", 2, 0, 0.999]
 network_content[12] = ["GDNF", "BDNF", 2, 0, 0.95]
-network_content.T.to_csv("network.tsv", sep="\t", index=None)
+network_content.T.to_csv(file_folder+"network.tsv", sep="\t", index=None)
 
 ## An experiment retrieved from LINCS L1000 involving those genes
 index=["PHOX2B","EDN3","RET","GDNF","BDNF","ASCL1","cell_line","annotation","perturbed","perturbation","sigid"]
 experiments_content = pd.DataFrame([], index=index)
 experiments_content["BDNF_KD_SHSY5Y"] = [1,1,1,0,0,0,"Cell","2","BDNF","KD","Sig1"] ## mutated profile
 experiments_content["initial_SHSY5Y"] = [1,1,1,1,0,0,"Cell","1","None","None","Sig2"] ## control/initial profile
-experiments_content.to_csv("experiments.csv")
+experiments_content.to_csv(file_folder+"experiments.csv")
 
+## Network identification
 from NORDic.NORDic_NI.functions import network_identification
 solution = network_identification(file_folder, taxon_id, path_to_genes=None, 
-    network_fname="network.tsv", experiments_fname="experiments.csv", 
+    network_fname=file_folder+"network.tsv", experiments_fname=file_folder+"experiments.csv", 
     disgenet_args=DISGENET_args, string_args=STRING_args, edge_args=EDGE_args, lincs_args=LINCS_args, 
     sig_args=SIG_args, bonesis_args=BONESIS_args, weights=DESIRABILITY, seed=seed_number, njobs=njobs,
     force_experiments=force_experiments, accept_nonRNA=accept_nonRNA, preserve_network_sign=preserve_network_sign)
@@ -76,28 +78,3 @@ assert os.path.exists(file_folder+'inferred_max_criterion_solution.png')
 with open(file_folder+"solution.bnet", "r") as f:
     network = f.read().split("\n")
 assert len(network)==6
-
-## II. Prioritization of master regulators (**NORDic PMR**)
-
-np.random.seed(12345)
-with open(file_folder+"solution.bnet", "r") as f:
-    genes = [line.split(", ")[0] for line in f.read().split("\n") if (len(line)>0)]
-state_len = 10 # number of patient states to generate
-states = pd.DataFrame(
-  [np.random.choice([0,1], p=[0.5,0.5], size=len(genes)).tolist() for _ in range(state_len)]
-  , columns=genes, index=["Patient %d" % (i+1) for i in range(state_len)]).T
-seed_number=12345
-njobs=min(5,max(1,cpu_count()-2))
-k=1
-IM_params = {"seed": seed_number, "gene_inputs": genes, "gene_outputs": genes}
-SIMU_params = {'nb_sims': 1000, 'rates': "fully_asynchronous", 'thread_count': njobs, 'depth': "constant_unitary"}
-
-from NORDic.NORDic_PMR.functions import greedy
-S, spreads = greedy(file_folder+"solution.bnet", k, states, IM_params, SIMU_params, save_folder=file_folder)
-assert spreads.shape[0]==6
-
-from subprocess import call
-call("rm -rf "+file_folder, shell=True)
-call("rm -f experiments.csv network.tsv", shell=True)from subprocess import call
-call("rm -rf "+file_folder, shell=True)
-call("rm -f experiments.csv network.tsv", shell=True)
