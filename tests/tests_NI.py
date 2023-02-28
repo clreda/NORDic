c2430e2b418a2b6a5f6a760fa7b0451b21babba3
REDA Clemence <clemence.reda@inria.fr>
2023-03-01 00:14:33 +0100

    Fix tests

--- tests/tests_NI.py
+++ tests/tests_NI.py
@@ -1,11 +1,7 @@
 #coding: utf-8
 
-#import sys, os
-#testdir = os.path.dirname(__file__)
-#srcdir = '../src/'
-#sys.path.insert(0, os.path.abspath(os.path.join(testdir, srcdir)))
-
 import NORDic
+import os
 import pandas as pd
 from multiprocessing import cpu_count
 import numpy as np
