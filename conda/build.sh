c6324fb7f1b433200d3efa351922cf4cd1771467
Clémence Réda <20943910+clreda@users.noreply.github.com>
2025-07-02 16:59:04 +0000

    update recipe for nordic 2.7

--- conda/build.sh
+++ conda/build.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
+set -xe
+
 cd ${SRC_DIR}"/MaBoSS-2.0/engine/src/"
 
-make install \
+make -j"${CPU_COUNT}" install \
     CC="${CC}" \
     CXX="${CXX}" \
     CFLAGS="${CFLAGS}" \
@@ -18,6 +20,6 @@ cd ${SRC_DIR}"/bonesis-0.6.7/"
 
 "${PYTHON}" -m pip install --no-deps --no-build-isolation . -vvv
 
-cd ${SRC_DIR}"/NORDic-2.6.0/" || exit 1
+cd ${SRC_DIR}"/NORDic-2.7.0/" || exit 1
 
 "${PYTHON}" -m pip install --no-deps --no-build-isolation . -vvv
